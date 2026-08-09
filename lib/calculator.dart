e180e62159d28b67db210b7844ab68f619be7afc
tarun225601-star <tarun225601@gmail.com>
2026-08-09 18:37:05 +0530

    Autonomous Agent Self-Verified Commit: lib/calculator.dart

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -6,58 +6,48 @@ class Calculator extends StatefulWidget {
   const Calculator({Key? key}) : super(key: key);
 
   @override
-  _CalculatorState createState() => _CalculatorState();
+  State<Calculator> createState() => _CalculatorState();
 }
 
 class _CalculatorState extends State<Calculator> {
-  String _expression = '';
+  String _display = '';
   String _result = '';
+  String _operator = '';
+  double _num1 = 0;
+  double _num2 = 0;
 
-  void _onButtonPressed(String value) {
-    setState(() {
-      if (value == '=') {
-        _result = _calculate(_expression);
-        _expression = '';
-      } else if (value == 'C') {
-        _expression = '';
-        _result = '';
-      } else {
-        _expression += value;
+  void _onClick(String value) {
+    if (value == '+' || value == '-' || value == '*' || value == '/') {
+      _num1 = double.parse(_display);
+      _operator = value;
+      _display = '';
+    } else if (value == '=') {
+      _num2 = double.parse(_display);
+      switch (_operator) {
+        case '+':
+          _result = (_num1 + _num2).toString();
+          break;
+        case '-':
+          _result = (_num1 - _num2).toString();
+          break;
+        case '*':
+          _result = (_num1 * _num2).toString();
+          break;
+        case '/':
+          _result = (_num1 / _num2).toString();
+          break;
       }
-    });
-  }
-
-  String _calculate(String expression) {
-    try {
-      return expression
-          .replaceAll(' ', '')
-          .split('
-')
-          .map((element) => element
-              .replaceAll('+', ' + ')
-              .replaceAll('-', ' - ')
-              .replaceAll('*', ' * ')
-              .replaceAll('/', ' / ')
-              .split(' ')
-              .map((e) => double.parse(e))
-              .reduce((value, element) {
-                if (expression.contains('+')) {
-                  return value + element;
-                } else if (expression.contains('-')) {
-                  return value - element;
-                } else if (expression.contains('*')) {
-                  return value * element;
-                } else if (expression.contains('/')) {
-                  return value / element;
-                } else {
-                  return 0;
-                }
-              }))
-          .join('
-');
-    } catch (e) {
-      return 'Error';
+      _display = _result;
+    } else if (value == 'C') {
+      _display = '';
+      _result = '';
+      _operator = '';
+      _num1 = 0;
+      _num2 = 0;
+    } else {
+      _display += value;
     }
+    setState(() {});
   }
 
   @override
@@ -65,85 +55,31 @@ class _CalculatorState extends State<Calculator> {
     return Scaffold(
       body: Column(
         children: [
-          CalculatorDisplay(
-            expression: _expression,
-            result: _result,
-          ),
-          const SizedBox(height: 20),
-          GridView.count(
-            shrinkWrap: true,
-            crossAxisCount: 4,
-            childAspectRatio: 1.2,
-            children: [
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '7',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '8',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '9',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '/',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '4',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '5',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '6',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '*',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '1',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '2',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '3',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '-',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '0',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '.',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '=',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: '+',
-              ),
-              CalculatorButton(
-                onPressed: _onButtonPressed,
-                value: 'C',
-              ),
-            ],
+          CalculatorDisplay(display: _display),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 4,
+              childAspectRatio: 1.2,
+              children: [
+                CalculatorButton(value: '7', onClick: _onClick),
+                CalculatorButton(value: '8', onClick: _onClick),
+                CalculatorButton(value: '9', onClick: _onClick),
+                CalculatorButton(value: '/', onClick: _onClick),
+                CalculatorButton(value: '4', onClick: _onClick),
+                CalculatorButton(value: '5', onClick: _onClick),
+                CalculatorButton(value: '6', onClick: _onClick),
+                CalculatorButton(value: '*', onClick: _onClick),
+                CalculatorButton(value: '1', onClick: _onClick),
+                CalculatorButton(value: '2', onClick: _onClick),
+                CalculatorButton(value: '3', onClick: _onClick),
+                CalculatorButton(value: '-', onClick: _onClick),
+                CalculatorButton(value: '0', onClick: _onClick),
+                CalculatorButton(value: '.', onClick: _onClick),
+                CalculatorButton(value: '=', onClick: _onClick),
+                CalculatorButton(value: '+', onClick: _onClick),
+                CalculatorButton(value: 'C', onClick: _onClick),
+              ],
+            ),
           ),
         ],
       ),
