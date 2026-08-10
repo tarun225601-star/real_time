5e94ac80159321dc9217c9beabd3aae650300aa0
tarun225601-star <tarun225601@gmail.com>
2026-08-10 14:47:29 +0530

    Autonomous Sync: lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,136 +1,24 @@
+
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/home_screen.dart';
+import 'package:instagram_clone/screens/feed_screen.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
+import 'package:instagram_clone/utils/constants.dart';
+import 'package:instagram_clone/utils/styles.dart';
+import 'package:instagram_clone/services/api_service.dart';
+import 'package:instagram_clone/services/storage_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const CalculatorPage(),
-    );
-  }
-}
-
-class CalculatorPage extends StatefulWidget {
-  const CalculatorPage({Key? key}) : super(key: key);
-
-  @override
-  State<CalculatorPage> createState() => _CalculatorPageState();
-}
-
-class _CalculatorPageState extends State<CalculatorPage> {
-  String _expression = '';
-  String _result = '';
-
-  void _onPressed(String value) {
-    setState(() {
-      if (value == '=') {
-        try {
-          _result = _calculate(_expression);
-        } catch (e) {
-          _result = 'Error';
-        }
-      } else if (value == 'C') {
-        _expression = '';
-        _result = '';
-      } else {
-        _expression += value;
-      }
-    });
-  }
-
-  String _calculate(String expression) {
-    try {
-      return expression
-          .replaceAll('Ã', '*')
-          .replaceAll('Ã·', '/')
-          .replaceAll('%', '/100')
-          .replaceAll(' ', '')
-          double.parse(expression).toStringAsFixed(2);
-
-    } catch (e) {
-      return 'Error';
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calculator'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    _expression,
-                    style: const TextStyle(fontSize: 24),
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    _result,
-                    style: const TextStyle(fontSize: 48),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: GridView.count(
-              crossAxisCount: 4,
-              childAspectRatio: 1.2,
-              children: [
-                _buildButton('7'),
-                _buildButton('8'),
-                _buildButton('9'),
-                _buildButton('Ã·'),
-                _buildButton('4'),
-                _buildButton('5'),
-                _buildButton('6'),
-                _buildButton('Ã'),
-                _buildButton('1'),
-                _buildButton('2'),
-                _buildButton('3'),
-                _buildButton('-'),
-                _buildButton('0'),
-                _buildButton('.'),
-                _buildButton('='),
-                _buildButton('+'),
-                _buildButton('C'),
-                _buildButton('%'),
-                _buildButton(''),
-                _buildButton(''),
-                _buildButton(''),
-                _buildButton(''),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildButton(String value) {
-    return ElevatedButton(
-      onPressed: () => _onPressed(value),
-      child: Text(
-        value,
-        style: const TextStyle(fontSize: 24),
-      ),
+      title: Constants.appTitle,
+      theme: Styles.theme,
+      home: HomeScreen(),
     );
   }
 }
