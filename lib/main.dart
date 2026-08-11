9e88dc70e0cdf9ed58f70798f2decf90acebc6b0
tarun225601-star <tarun225601@gmail.com>
2026-08-11 13:34:49 +0530

    Autonomous Agent: Add/Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'My App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
