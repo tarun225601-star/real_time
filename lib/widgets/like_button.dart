ce1b2e64e0539f7ac8346a80bb6ede9a3f32d0a2
tarun225601-star <tarun225601@gmail.com>
2026-08-12 11:46:15 +0530

    Autonomous Agent: Add/Update lib/widgets/like_button.dart

--- lib/widgets/like_button.dart
+++ lib/widgets/like_button.dart
@@ -1,34 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/models/video_model.dart';
-import 'package:instagram_clone/utils/shared_preferences.dart';
 
 class LikeButton extends StatefulWidget {
-  final VideoModel video;
-
-  LikeButton({required this.video});
-
   @override
   _LikeButtonState createState() => _LikeButtonState();
 }
 
 class _LikeButtonState extends State<LikeButton> {
   bool _isLiked = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkIfLiked();
-  }
-
-  _checkIfLiked() async {
-    final likedVideos = await SharedPreferencesUtil.getLikedVideos();
-    if (likedVideos.contains(widget.video.id)) {
-      setState(() {
-        _isLiked = true;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return IconButton(
@@ -36,11 +14,6 @@ class _LikeButtonState extends State<LikeButton> {
         _isLiked ? Icons.favorite : Icons.favorite_border,
       ),
       onPressed: () {
-        if (_isLiked) {
-          SharedPreferencesUtil.removeLikedVideo(widget.video.id);
-        } else {
-          SharedPreferencesUtil.addLikedVideo(widget.video.id);
-        }
         setState(() {
           _isLiked = !_isLiked;
         });
