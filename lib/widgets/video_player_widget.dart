b0ebaf7b66b048517db67c14667de9a0c448611d
tarun225601-star <tarun225601@gmail.com>
2026-08-12 11:59:39 +0530

    Autonomous Agent: Add/Update lib/widgets/video_player_widget.dart

--- lib/widgets/video_player_widget.dart
+++ lib/widgets/video_player_widget.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:instagram_clone/models/video_model.dart';
 
 class VideoPlayerWidget extends StatefulWidget {
-  final String videoUrl;
-  final VoidCallback onTap;
-  VideoPlayerWidget({required this.videoUrl, required this.onTap});
+  final VideoModel video;
+
+  VideoPlayerWidget({required this.video});
+
   @override
   _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
 }
 
 class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-  late VideoPlayerController _videoPlayerController;
+  late VideoPlayerController _controller;
+
   @override
   void initState() {
     super.initState();
-    _videoPlayerController = VideoPlayerController.network(
-      widget.videoUrl,
-    )..initialize().then((_) {
-      setState(() {});
-    });
+    _controller = VideoPlayerController.network(widget.video.url)
+      ..initialize().then((_) {
+        setState(() {});
+      });
   }
+
   @override
   void dispose() {
-    _videoPlayerController.dispose();
+    _controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: widget.onTap,
-      child: VideoPlayer(_videoPlayerController),
-    );
+    return _controller.value.isInitialized
+        ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+        : Center(
+            child: CircularProgressIndicator(),
+          );
   }
 }
