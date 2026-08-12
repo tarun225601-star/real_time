d03f3e9003f067640c9d9dd180a553209e792f5b
tarun225601-star <tarun225601@gmail.com>
2026-08-12 13:50:15 +0530

    Autonomous Agent: Fresh Full Write lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,154 +26,41 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
-  final _videoController = VideoPlayerController.asset('assets/video.mp4');
-  bool _isLiked = false;
-  bool _isCommentSheetOpen = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _videoController.initialize().then((_) {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    _videoController.dispose();
-    super.dispose();
-  }
+  final List<Video> _videos = [
+    Video(
+      id: 1,
+      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      creatorName: 'Creator 1',
+      caption: 'This is a sample video',
+      musicTitle: 'Sample Music',
+    ),
+    Video(
+      id: 2,
+      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+      creatorName: 'Creator 2',
+      caption: 'This is another sample video',
+      musicTitle: 'Another Sample Music',
+    ),
+    Video(
+      id: 3,
+      videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+      creatorName: 'Creator 3',
+      caption: 'This is yet another sample video',
+      musicTitle: 'Yet Another Sample Music',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          PageView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Stack(
-                children: [
-                  VideoPlayer(_videoController),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          colors: [Colors.black, Colors.transparent],
-                        ),
-                      ),
-                      child: Row(
-                        children: [
-                          const Text(
-                            'Creator Name',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          const SizedBox(width: 8),
-                          const Text(
-                            'Caption',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          const SizedBox(width: 8),
-                          const Text(
-                            'Music Title',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 0,
-                    right: 0,
-                    child: IconButton(
-                      icon: const Icon(Icons.person),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ProfilePage()),
-                        );
-                      },
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Column(
-                      children: [
-                        IconButton(
-                          icon: Icon(
-                            _isLiked ? Icons.favorite : Icons.favorite_border,
-                            color: _isLiked ? Colors.red : Colors.white,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _isLiked = !_isLiked;
-                            });
-                          },
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.comment, color: Colors.white),
-                          onPressed: () {
-                            setState(() {
-                              _isCommentSheetOpen = true;
-                            });
-                          },
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.share, color: Colors.white),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-          if (_isCommentSheetOpen)
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16),
-                    topRight: Radius.circular(16),
-                  ),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Write a comment',
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _isCommentSheetOpen = false;
-                        });
-                      },
-                      child: const Text('Post'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-        ],
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: _videos.length,
+        itemBuilder: (context, index) {
+          return VideoPlayerScreen(
+            video: _videos[index],
+          );
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
@@ -183,13 +70,137 @@ class _MyHomePageState extends State<MyHomePage> {
           });
         },
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.upload), label: 'Upload'),
-          BottomNavigationBarItem(icon: Icon(Icons.inbox), label: 'Inbox'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.upload),
+            label: 'Upload',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inbox),
+            label: 'Inbox',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
         ],
       ),
+    );
+  }
+}
+
+class VideoPlayerScreen extends StatefulWidget {
+  final Video video;
+
+  const VideoPlayerScreen({Key? key, required this.video}) : super(key: key);
+
+  @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+  bool _isLiked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.video.videoUrl)
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        _controller.value.isInitialized
+            ? VideoPlayer(_controller)
+            : const Center(child: CircularProgressIndicator()),
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.person),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ProfilePage()),
+                  );
+                },
+              ),
+              const Spacer(),
+              IconButton(
+                icon: Icon(
+                  _isLiked ? Icons.favorite : Icons.favorite_border,
+                  color: _isLiked ? Colors.red : Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _isLiked = !_isLiked;
+                  });
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.comment),
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => const CommentSheet(),
+                  );
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.share),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            color: Colors.black.withOpacity(0.5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.video.creatorName,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                Text(
+                  widget.video.caption,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                Text(
+                  widget.video.musicTitle,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -199,10 +210,52 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
+    return const Scaffold(
+      body: Center(
         child: Text('Profile Page'),
       ),
     );
   }
+}
+
+class CommentSheet extends StatelessWidget {
+  const CommentSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Write a comment',
+            ),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Post'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Video {
+  final int id;
+  final String videoUrl;
+  final String creatorName;
+  final String caption;
+  final String musicTitle;
+
+  Video({
+    required this.id,
+    required this.videoUrl,
+    required this.creatorName,
+    required this.caption,
+    required this.musicTitle,
+  });
 }
