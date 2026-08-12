3ba110329521df6ed7987835531ce6a8ffafb705
tarun225601-star <tarun225601@gmail.com>
2026-08-12 20:13:59 +0530

    Autonomous Agent: Add/Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Real Time App',
+      title: 'Calculator App',
       home: CalculatorPage(),
     );
   }
