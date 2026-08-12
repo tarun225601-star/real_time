1788119ad1aa6c57df9f4b6286943d668d24d2fc
tarun225601-star <tarun225601@gmail.com>
2026-08-12 11:59:38 +0530

    Autonomous Agent: Add/Update lib/utils/constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,6 @@
 class Constants {
-  static const String VIDEO_URL = 'https://example.com/video.mp4';
+  static const String BASE_URL = 'https://example.com/api';
+  static const String VIDEO_URL = 'https://example.com/video';
+  static const String IMAGE_URL = 'https://example.com/image';
+  static const String CACHE_DIR = 'cache';
 }
