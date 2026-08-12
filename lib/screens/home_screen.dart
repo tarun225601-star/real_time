161238dcd618c9975917fb010d8378e1c6dfd9b2
tarun225601-star <tarun225601@gmail.com>
2026-08-12 11:46:08 +0530

    Autonomous Agent: Add/Update lib/screens/home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/models/video_model.dart';
+import 'package:instagram_clone/widgets/video_player_widget.dart';
 import 'package:instagram_clone/utils/constants.dart';
-import 'package:instagram_clone/widgets/video_player.dart';
-import 'package:instagram_clone/screens/video_screen.dart';
+import 'package:instagram_clone/screens/video_player_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,49 +9,26 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<VideoModel> _videos = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadVideos();
-  }
-
-  _loadVideos() async {
-    final videos = await VideoUrls.getVideos();
-    setState(() {
-      _videos = videos;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Instagram Clone'),
       ),
-      body: _videos.isEmpty
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: _videos.length,
-              itemBuilder: (context, index) {
-                return VideoPlayer(
-                  video: _videos[index],
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => VideoScreen(
-                          video: _videos[index],
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return VideoPlayerWidget(
+            videoUrl: 'https://example.com/video.mp4',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => VideoPlayerScreen()),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
