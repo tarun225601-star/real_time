c46634597332130475d9c31e25e795ad7c223205
tarun225601-star <tarun225601@gmail.com>
2026-08-12 11:34:01 +0530

    Autonomous Agent: Add/Update lib/screens/home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,58 @@
-import 'flowers/flash_app.dat';base64 encode the home_screns.dat file.
-class HomeScreen {
-    path path;
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/video_model.dart';
+import 'package:instagram_clone/utils/constants.dart';
+import 'package:instagram_clone/widgets/video_player.dart';
+import 'package:instagram_clone/screens/video_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<VideoModel> _videos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadVideos();
+  }
+
+  _loadVideos() async {
+    final videos = await VideoUrls.getVideos();
+    setState(() {
+      _videos = videos;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Instagram Clone'),
+      ),
+      body: _videos.isEmpty
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _videos.length,
+              itemBuilder: (context, index) {
+                return VideoPlayer(
+                  video: _videos[index],
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => VideoScreen(
+                          video: _videos[index],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+    );
+  }
 }
