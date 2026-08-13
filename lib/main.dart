35175f065bee5c55148de3663831a878c3b4ddc3
tarun225601-star <tarun225601@gmail.com>
2026-08-13 15:24:24 +0530

    Autonomous Agent: Add/Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,225 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+    const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'VibeTok',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color(0xFF0B132B),
-      ),
-      home: const MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return const MaterialApp(
+            title: 'app',
+            home: MyHomePage(),
+        );
+    }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+    const MyHomePage({Key? key}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-
-  final List<Widget> _children = [
-    const VideoFeed(),
-    const UserProfile(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // यहाँ SafeArea और Expanded लगाने से लेआउट कभी क्रैश नहीं होगा
-      body: SafeArea(
-        child: IndexedStack(
-          index: _currentIndex,
-          children: _children,
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color(0xFF0B132B),
-        selectedItemColor: const Color(0xFF00F5D4),
-        unselectedItemColor: Colors.white54,
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.video_call),
-            label: 'Feed',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class VideoFeed extends StatefulWidget {
-  const VideoFeed({Key? key}) : super(key: key);
-
-  @override
-  State<VideoFeed> createState() => _VideoFeedState();
-}
-
-class _VideoFeedState extends State<VideoFeed> {
-  final PageController _pageController = PageController();
-  int _currentPage = 0;
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // SizedBox.expand देने से PageView को पूरी स्क्रीन की सही हाइट मिल जाएगी और क्रैश नहीं होगा
-    return SizedBox.expand(
-      child: PageView.builder(
-        controller: _pageController,
-        scrollDirection: Axis.vertical,
-        onPageChanged: (page) {
-          setState(() {
-            _currentPage = page;
-          });
-        },
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                height: double.infinity,
-                color: Colors.black87,
-                child: Center(
-                  child: Text(
-                    'Video Player $index',
-                    style: const TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                right: 16,
-                bottom: 80,
-                child: Column(
-                  children: [
-                    IconButton(
-                      icon: const Icon(
-                        Icons.favorite,
-                        color: Color(0xFF00F5D4),
-                        size: 32,
-                      ),
-                      onPressed: () {},
-                    ),
-                    const SizedBox(height: 16),
-                    IconButton(
-                      icon: const Icon(
-                        Icons.person,
-                        color: Color(0xFF00F5D4),
-                        size: 32,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
-}
-
-class UserProfile extends StatelessWidget {
-  const UserProfile({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFF0B132B),
-      child: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 24),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.grey,
-              ),
-              child: const Center(
-                child: Text(
-                  'Avatar',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text('app'),
             ),
-            const SizedBox(height: 16),
-            const Text(
-              'Tarun User',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
+            body: const Center(
+                child: Text('app'),
             ),
-            const SizedBox(height: 24),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 3,
-                childAspectRatio: 1,
-                mainAxisSpacing: 4,
-                crossAxisSpacing: 4,
-                children: List.generate(
-                  12,
-                  (index) => Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white12,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Post $index',
-                        style: const TextStyle(
-                          fontSize: 14,
-                          color: Colors.white70,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+        );
+    }
 }
