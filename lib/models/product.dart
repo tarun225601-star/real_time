7be901f7ad8cf05fb5fe9b5a1d49919b50c141df
tarun225601-star <tarun225601@gmail.com>
2026-08-13 18:27:13 +0530

    Autonomous Laborer: Update lib/models/product.dart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,8 +1,7 @@
 class Product {
   final String id;
   final String name;
-  final String description;
   final double price;
 
-  Product({required this.id, required this.name, required this.description, required this.price});
+  Product({required this.id, required this.name, required this.price});
 }
