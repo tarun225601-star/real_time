8e48a72f1a18cfd181390ec1f6c2e3814fc8d14d
tarun225601-star <tarun225601@gmail.com>
2026-08-13 18:27:09 +0530

    Autonomous Laborer: Update lib/app_theme.dart

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
-    primaryColor: const Color(0xFF2196F3),
-    scaffoldBackgroundColor: const Color(0xFFFFFFFF),
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Color(0xFF2196F3),
-      foregroundColor: Color(0xFFFFFFFF),
-    ),
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+  );
+
+  static final darkTheme = ThemeData(
+    primarySwatch: Colors.grey,
+    scaffoldBackgroundColor: Colors.black,
   );
 }
