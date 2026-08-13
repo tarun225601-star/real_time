54db42c803526ca73c1d887d8e9639499ff7b13c
tarun225601-star <tarun225601@gmail.com>
2026-08-13 18:27:11 +0530

    Autonomous Laborer: Update lib/screens/home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:real_time/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,8 +10,14 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Marketplace App'),
       ),
-      body: const Center(
-        child: Text('Welcome to Marketplace App'),
+      body: Center(
+        child: Text(
+          'Welcome to Marketplace App',
+          style: TextStyle(
+            fontSize: 24,
+            color: AppTheme.lightTheme.primaryColor,
+          ),
+        ),
       ),
     );
   }
