35f26679683cad2105b648062ec0b4a5f7c0e087
tarun225601-star <tarun225601@gmail.com>
2026-08-13 17:24:50 +0530

    Autonomous Laborer: Update lib/utils/constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,4 @@
 class Constants {
-  static const String BASE_URL = 'https://example.com/api';
-  static const String VIDEO_URL = 'https://example.com/video';
-  static const String IMAGE_URL = 'https://example.com/image';
-  static const String CACHE_DIR = 'cache';
+  static const String appName = 'Marketplace App';
+  static const String baseUrl = 'https://example.com/api';
 }
