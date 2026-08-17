6a85db7979c29462619ae0352bcb9c98021f9c1d
tarun225601-star <tarun225601@gmail.com>
2026-08-17 12:54:06 +0530

    Self-Healing Engine: Update lib/models/calculator_model.dart

---
+++ lib/models/calculator_model.dart
@@ -0,0 +1,7 @@
+class CalculatorModel {
+  String? firstNumber;
+  String? secondNumber;
+  String? operation;
+  String? result;
+  CalculatorModel({this.firstNumber, this.secondNumber, this.operation, this.result});
+}
