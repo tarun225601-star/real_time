be3d34809f108d1400a2fd6850051ab18a302774
tarun225601-star <tarun225601@gmail.com>
2026-08-17 13:43:16 +0530

    Artifact Engine: Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,18 @@
-import 'package:calculator_app/views/home_screen.dart';
+import 'package:real_time/views/calculator_view.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:calculator_app/controllers/calculator_controller.dart';
+
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => CalculatorController()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Calculator App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen(),
+      home: CalculatorView(),
     );
   }
 }
