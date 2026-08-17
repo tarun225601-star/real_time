92436db0691e18501e100959168405771ac3991c
tarun225601-star <tarun225601@gmail.com>
2026-08-17 12:49:06 +0530

    Self-Healing Engine: Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,141 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:calculator_app/providers/calculator_provider.dart';
+import 'package:calculator_app/views/home_screen.dart';
 
 void main() {
-  runApp(CalculatorApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CalculatorProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
-class CalculatorApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
+      title: 'Calculator App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CalculatorHomePage(title: 'Calculator'),
-    );
-  }
-}
-
-class CalculatorHomePage extends StatefulWidget {
-  final String title;
-
-  CalculatorHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  _CalculatorHomePageState createState() => _CalculatorHomePageState();
-}
-
-class _CalculatorHomePageState extends State<CalculatorHomePage> {
-  String _output = '0';
-  double _num1 = 0.0;
-  double _num2 = 0.0;
-  String _operator = '';
-
-  void _buttonPressed(String buttonText) {
-    if (buttonText == 'C') {
-      _output = '0';
-      _num1 =.0;
-      _num2 =.0;
-      _operator = '';
-    } else if (buttonText == '+' || buttonText == '-' || buttonText == '*' || buttonText == '/') {
-      _num1 = double.parse(_output);
-      _operator = buttonText;
-      _output = '0';
-    } else if (buttonText == '.') {
-      if (_output.contains('.')) {
-        return;
-      }
-      _output += buttonText;
-    } else if (buttonText == '=') {
-      _num2 = double.parse(_output);
-
-      if (_operator == '+') {
-        _output = (_num1 + _num2).toString();
-      } else if (_operator == '-') {
-        _output = (_num1 - _num2).toString();
-      } else if (_operator == '*') {
-        _output = (_num1 * _num2).toString();
-      } else if (_operator == '/') {
-        _output = (_num1 / _num2).toString();
-      }
-
-      _num1 =.0;
-      _num2 =.0;
-      _operator = '';
-    } else {
-      _output = _output == '0' ? buttonText : _output + buttonText;
-    }
-
-    setState(() {});
-  }
-
-  Widget _buildButton(String buttonText) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ElevatedButton(
-          onPressed: () => _buttonPressed(buttonText),
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.all(24.0),
-            textStyle: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-          ),
-          child: Text(buttonText),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
-            child: Text(
-              _output,
-              style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Expanded(
-            child: Divider(),
-          ),
-          Column(children: [
-            Row(children: [
-              _buildButton('7'),
-              _buildButton('8'),
-              _buildButton('9'),
-              _buildButton('/'),
-            ]),
-            Row(children: [
-              _buildButton('4'),
-              _buildButton('5'),
-              _buildButton('6'),
-              _buildButton('*'),
-            ]),
-            Row(children: [
-              _buildButton('1'),
-              _buildButton('2'),
-              _buildButton('3'),
-              _buildButton('-'),
-            ]),
-            Row(children: [
-              _buildButton('.'),
-              _buildButton('0'),
-              _buildButton('C'),
-              _buildButton('+'),
-            ]),
-            Row(children: [
-              _buildButton('='),
-            ]),
-          ])
-        ],
-      ),
+      home: HomeScreen(),
     );
   }
 }
