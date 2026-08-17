a0d670cf9a854eaff28b5e8ad245db6886497d1b
tarun225601-star <tarun225601@gmail.com>
2026-08-17 13:43:22 +0530

    Artifact Engine: Update lib/controllers/calculator_controller.dart

--- lib/controllers/calculator_controller.dart
+++ lib/controllers/calculator_controller.dart
@@ -1,31 +1,23 @@
-import 'package:calculator_app/models/calculator_model.dart';
-import 'package:calculator_app/services/calculator_service.dart';
-import 'package:flutter/material.dart';
-class CalculatorController with ChangeNotifier {
-  CalculatorModel _calculatorModel = CalculatorModel();
-  CalculatorService _calculatorService = CalculatorService();
-  String? get firstNumber => _calculatorModel.firstNumber;
-  String? get secondNumber => _calculatorModel.secondNumber;
-  String? get operation => _calculatorModel.operation;
-  String? get result => _calculatorModel.result;
-  void setFirstNumber(String value) {
-    _calculatorModel.firstNumber = value;
-    notifyListeners();
+import 'package:real_time/models/calculator_model.dart';
+
+abstract class CalculatorController {
+  String calculate(String expression);
+}
+
+class CalculatorControllerImpl implements CalculatorController {
+  @override
+  String calculate(String expression) {
+    try {
+      return (eval(expression)).toString();
+    } catch (e) {
+      return 'Error';
+    }
   }
-  void setSecondNumber(String value) {
-    _calculatorModel.secondNumber = value;
-    notifyListeners();
-  }
-  void setOperation(String value) {
-    _calculatorModel.operation = value;
-    notifyListeners();
-  }
-  void calculateResult() {
-    _calculatorModel.result = _calculatorService.calculate(
-      double.parse(_calculatorModel.firstNumber!),
-      double.parse(_calculatorModel.secondNumber!),
-      _calculatorModel.operation!,
-    );
-    notifyListeners();
+}
+
+class CalculatorController extends CalculatorControllerImpl implements CalculatorController {
+  @override
+  String calculate(String expression) {
+    return super.calculate(expression);
   }
 }
